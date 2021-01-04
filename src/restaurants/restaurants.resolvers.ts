b2d0531be2edcd02fcd3f467e030b6925d1a3f11
DevKang <olddeapo@gmail.com>
2021-01-04 20:12:46 +0900

    [WIP] #3.7

--- src/restaurants/restaurants.resolvers.ts
+++ src/restaurants/restaurants.resolvers.ts
@@ -1,11 +1,12 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantService } from './restaurants.service';
 
 @Resolver(() => Restaurant)
 export class RestaurantResolver {
-  constructor(private readonly restaurantService: RestaurantService) {}
+  constructor(private readonly restaurantService: RestaurantService) { }
 
   @Query(() => [Restaurant])
   restaurants(): Promise<Restaurant[]> {
@@ -24,4 +25,9 @@ export class RestaurantResolver {
       return false;
     }
   }
+
+  @Mutation(() => Boolean)
+  async updateRestaurant(
+    @Args('input') updateRestaurantDto: UpdateRestaurantDto,
+  ) { }
 }
